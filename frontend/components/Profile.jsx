a5502883b262d60c20231208b4812bd00883b0ca
Samnisa <samnisa2104@gmail.com>
2025-01-19 11:25:34 +0700

    I don't know

--- frontend/components/Profile.jsx
+++ frontend/components/Profile.jsx
@@ -132,7 +132,7 @@ export default function ProfileUser({ id }) {
         {/* Mobile Navigation Links */}
         {isMenuOpen && (
           <div className="flex flex-col md:hidden bg-white shadow-md py-2">
-            <Link href="/profile" className="px-4 py-2 hover:bg-gray-100">
+            <Link href="/" className="px-4 py-2 hover:bg-gray-100">
               Home
             </Link>
             <Link href="/services" className="px-4 py-2 hover:bg-gray-100">
